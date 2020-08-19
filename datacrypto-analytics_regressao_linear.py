205e047a1d62ba7979fe4bf71a28dbb3ad33adb4
datacryptoanalytics <datacryptoanalytics@protonmail.com>
2020-08-19 01:09:30 -0400

    troca do gráfico.

--- datacrypto-analytics_regressao_linear.py
+++ datacrypto-analytics_regressao_linear.py
@@ -6,34 +6,34 @@ Created on Sat Aug 31 15:20:28 2019
 @author: Felipe Soares
 """
 
-import requests           
-import json               
-import pandas as pd       
-import datetime as dt     
-from matplotlib import pyplot as plt 
+import requests
+import json
+import pandas as pd
+import datetime as dt
+from matplotlib import pyplot as plt
 
 
-#Esolhe a Criptomoedas e valores de negociação
-criptomoeda = input('========= DataCrypto Analytics =========' 
-                    '\n\nDigite o par de criptomoedas listada na Binance:')
+criptomoeda = input('DataCrypto Analytics | Algoritmo XGBoost |'
+                    '\n\n | Twitter @DataCryptoML |'
+                    '\n | Github @datacryptoanalytics |'
+                    '\n \nDigite o par de criptomoedas listada na Binance: ')
 
-print('O par de criptomoeda informada foi: %s' 
-      '\n \nDataCrypto Analytics esta buscando os valores,' 
-      'por favor aguarde alguns segundos!' %(criptomoeda))
+print('\nO par de criptomoeda informada foi: %s'
+      '\n\nDataCrypto Analytics esta buscando os valores,'
+      ' por favor aguarde alguns segundos!' %(criptomoeda))
 
 
-#Cria URL da API Binance
 root_url = 'https://api.binance.com/api/v1/klines'
 
 symbol = criptomoeda
 
-interval = '1D'
+interval = input('Digite o Timeframe (Exemplo: 15m, 30m, 1h, 1d, 1M): ')
 
 url = root_url + '?symbol=' + symbol + '&interval=' + interval
 print(url)
 
 #Monta URL e o Gráfico e DataFrane
-def get_bars(symbol, interval = '1d'):
+def get_bars(symbol, interva = interval):
    url = root_url + '?symbol=' + symbol + '&interval=' + interval
    data = json.loads(requests.get(url).text)
    df = pd.DataFrame(data)
@@ -46,9 +46,9 @@ def get_bars(symbol, interval = '1d'):
 
 criptomoeda = get_bars(criptomoeda)
 
-criptomoeda_fechamento = criptomoeda['c'].astype('float').values 
+criptomoeda_fechamento = criptomoeda['c'].astype('float').values
 criptomoeda_abertura = criptomoeda['o'].astype('float').values
-criptomoeda_num_trades = criptomoeda['num_trades'].astype('float') 
+criptomoeda_num_trades = criptomoeda['num_trades'].astype('float')
 criptomoeda_maxima = criptomoeda['h'].astype('float')
 criptomoeda_minima = criptomoeda['l'].astype('float')
 criptomoeda_volume = criptomoeda['v'].astype('float')
@@ -57,8 +57,7 @@ criptomoeda_datas_abertura = criptomoeda['open_time'].astype('float')
 taker_base_vol = criptomoeda['taker_base_vol'].astype('float')
 taker_quote_vol = criptomoeda['taker_quote_vol'].astype('float')
 
-
-
+#============
 import numpy as np
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
@@ -93,11 +92,21 @@ print("Mean squared error: %.2f"
 print('Score de variância(próximo de 1.0 bom > ruim): %.2f'
       % r2_score(y_test, criptomoeda_y_pred))
 
-# Plot outputs
-plt.figure(figsize=(9,5))
-plt.title('DataCrypto Analytics')
-plt.style.use('dark_background')
-plt.style.use('fivethirtyeight')
-plt.scatter(X_test, y_test,  color='g')
-plt.plot(X_test, criptomoeda_y_pred, color='red', linewidth=3)
+#============  Criar Gráfico
+plt.style.use('bmh')
+plt.rcParams['figure.figsize'] = (9,5)
+
+plt.subplot(2, 1, 1)
+plt.plot(criptomoeda_fechamento, '-', color="black")
+plt.legend(['Close'], loc=0)
+plt.title('')
+plt.ylabel('Price')
+
+plt.subplot(2, 1, 2)
+plt.scatter(X_test, y_test, color="black")
+plt.plot(X_test, criptomoeda_y_pred, color='red', linewidth=2)
+plt.legend(['linear regression' ], loc=0)
+plt.xlabel('')
+plt.ylabel('Linear Regression')
+
 plt.show()
