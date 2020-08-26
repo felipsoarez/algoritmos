7e3ce08c9bf81d3066c6dce50577d193d3503c6d
datacryptoanalytics <datacryptoanalytics@protonmail.com>
2020-08-26 16:31:47 -0400

    altteração simples

--- social-sentiment-twitter.py
+++ social-sentiment-twitter.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Script para buscar sentimento de alguma criptomoeda no twitter
-
 @author: Felipe Ssoares
 """
 
@@ -20,7 +19,7 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-#Variável que irá armazenar todos os Tweets com 
+#Variável que irá armazenar todos os Tweets com
 #a palavra escolhida na função search da API
 pesquisa = input(
         '========= DataCrypto Analytics ========='
@@ -39,6 +38,3 @@ for tweet in public_tweets:
 
 print('================================='
       '\n\nA média calculada foi de: ' + str(np.mean(tweets)))
-
-
-        
