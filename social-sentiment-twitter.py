7298a9903d6c37dec9ab166fe79071c720fac79f
datacryptoanalytics <datacryptoanalytics@protonmail.com>
2020-08-26 16:34:55 -0400

    alteração inicio algoritmo

--- social-sentiment-twitter.py
+++ social-sentiment-twitter.py
@@ -4,7 +4,6 @@
 Script para buscar sentimento de alguma criptomoeda no twitter
 @author: Felipe Ssoares
 """
-
 from textblob import TextBlob as tb
 import tweepy
 import numpy as np
@@ -21,10 +20,13 @@ api = tweepy.API(auth)
 
 #Variável que irá armazenar todos os Tweets com
 #a palavra escolhida na função search da API
-pesquisa = input(
-        '========= DataCrypto Analytics ========='
-        '\n\nQual palavra deseja calcular o sentimento no Twitter?'
-        '\n\nDigite aqui por favor:')
+pesquisa = input('DataCrypto Analytics | Linear Regression |'
+                    '\n\n | Twitter @DataCryptoML |'
+                    '\n | Github @datacryptoanalytics |'
+                    '\n\nQual palavra deseja calcular o sentimento no Twitter?'
+                    '\n\nDigite aqui por favor:')
+
+
 public_tweets = api.search(pesquisa)
 
 tweets = [] # Lista vazia para armazenar scores
